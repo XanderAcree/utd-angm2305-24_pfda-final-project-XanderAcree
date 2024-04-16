f307449e529eae803708f4e94d00adf53a5eeecc
XanderAcree <xca220000@utdallas.edu>
2024-04-16 16:15:57 -0500

    imported pygame & random, added pygame.init and width/height and screen, added pygame custom font, and established EldenRandom class (with __init__ function)

--- eldenRandomizer.py
+++ eldenRandomizer.py
@@ -1,7 +1,39 @@
 import pygame
+from pygame.locals import *
+import random
+
+# pygame setup
+pygame.init()
+width, height = 1920, 1080
+screen = pygame.display.set_mode((width, height))
+
+# setting font
+font = pygame.font.Font("fonts\Mantinia Regular.otf", 48)
+
+class EldenRandom():
+    def __init__(self, randAttribute = random.randrange(0,255), randColor = pygame.Color(0,0,0), randCol_r = pygame.Color.r(0), randCol_g = pygame.Color.g(0), randCol_b = pygame.Color.b(0)):
+        self.randAttribute = randAttribute
+        self.randColor = randColor
+        self.randCol_r = randCol_r
+        self.randCol_g = randCol_g
+        self.randCol_b = randCol_b
+        
+
 
 def main():
-    print("Hello World!")
+    # Game loop
+    running = True
+    while running:
+
+        # Event Handler
+        for event in pygame.event.get():
+            # Quit Game
+            if event.type == pygame.QUIT:
+                running = False
+        
+        pygame.display.update()
+    
+    pygame.QUIT
 
 if __name__ == "__main__":
     main()
